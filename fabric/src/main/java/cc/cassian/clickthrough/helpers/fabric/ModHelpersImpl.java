61efb45637b7f7acc6a03c03f5b5b06e64bf3dd7
Cassian Godsted <cassian@godsted.com>
2024-12-27 16:11:14 -0500

    Allowing for clicking through to modded chests

--- fabric/src/main/java/cc/cassian/clickthrough/helpers/fabric/ModHelpersImpl.java
+++ fabric/src/main/java/cc/cassian/clickthrough/helpers/fabric/ModHelpersImpl.java
@@ -1,9 +1,15 @@
 package cc.cassian.clickthrough.helpers.fabric;
 
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBlockTags;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.BlockState;
 
 public class ModHelpersImpl {
     public static boolean clothConfigInstalled() {
         return FabricLoader.getInstance().isModLoaded("cloth-config");
+    }
+
+    public static boolean isTaggedAsContainer(BlockState state) {
+        return state.isIn(ConventionalBlockTags.CHESTS) || state.isIn(ConventionalBlockTags.BARRELS);
     }
 }
--- neoforge/src/main/java/cc/cassian/clickthrough/helpers/neoforge/ModHelpersImpl.java
+++ neoforge/src/main/java/cc/cassian/clickthrough/helpers/neoforge/ModHelpersImpl.java
@@ -1,9 +1,15 @@
 package cc.cassian.clickthrough.helpers.neoforge;
 
+import net.minecraft.block.BlockState;
 import net.neoforged.fml.ModList;
+import net.neoforged.neoforge.common.Tags;
 
 public class ModHelpersImpl {
     public static boolean clothConfigInstalled() {
         return ModList.get().isLoaded("cloth_config");
+    }
+
+    public static boolean isTaggedAsContainer(BlockState state) {
+        return state.isIn(Tags.Blocks.CHESTS) || state.isIn(Tags.Blocks.BARRELS);
     }
 }
